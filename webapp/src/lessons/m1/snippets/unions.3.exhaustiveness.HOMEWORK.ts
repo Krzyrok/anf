d62c491659836c81c7d600cdc0fb6614b5bdd5f6
Krzyrok <krzyrok@google.com>
2022-07-06 21:24:29 +0200

    unions.3.exhaustiveness.HOMEWORK.ts homework

--- webapp/src/lessons/m1/snippets/unions.3.exhaustiveness.HOMEWORK.ts
+++ webapp/src/lessons/m1/snippets/unions.3.exhaustiveness.HOMEWORK.ts
@@ -16,24 +16,36 @@ type Tax_DE = 0 | 0.07 | 0.19 // 0%, 7%, 19%
 type Tax_FR = 0 | 0.021 | 0.055 | 0.1 | 0.2 // 0%, 2.1%, 5.5%, 10%, 20%
 type Tax_UK = 0 | 0.05 | 0.20 // 0%, 5%, 20%
 
-// ten typ jest za szeroki...
-// bo pozwala na połączenie dowolnej stawki podatku z dowolnym krajem
-// a chcemy, aby dopuszczalne były tylko te
-type Purchase = {
-  country: Country
-  vatTax: number
+interface CommonPurchase {
   name: string
   netPrice: number
 }
 
+// ten typ jest za szeroki...
+// bo pozwala na połączenie dowolnej stawki podatku z dowolnym krajem
+// a chcemy, aby dopuszczalne były tylko te
+type Purchase = {
+  country: 'PL'
+  vatTax: Tax_PL
+} & CommonPurchase | {
+  country: 'DE'
+  vatTax: Tax_DE
+} & CommonPurchase | {
+  country: 'FR'
+  vatTax: Tax_FR
+} & CommonPurchase | {
+  country: 'UK'
+  vatTax: Tax_UK
+} & CommonPurchase
+
 const purchases: Purchase[] = [{
   country: 'PL',
-  vatTax: 0.055,
+  vatTax: 0.05,
   name: 'dumplings',
   netPrice: 100,
 }, {
   country: 'DE',
-  vatTax: 0.1,
+  vatTax: 0.07,
   name: 'bawarian beer',
   netPrice: 200,
 }, {
@@ -43,7 +55,7 @@ const purchases: Purchase[] = [{
   netPrice: 300,
 }, {
   country: 'UK',
-  vatTax: 0.23,
+  vatTax: 0.2,
   name: 'eggs & bacon',
   netPrice: 400,
 }]
