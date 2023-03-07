6a412f49a8a567b16628c9c7664be38891135e96
Krzyrok <krzyrok@google.com>
2023-03-07 09:05:14 +0100

    add comment about differences between BE and FE model

--- webapp/src/lessons/m1/homework-design/done-homework/domain/question.ts
+++ webapp/src/lessons/m1/homework-design/done-homework/domain/question.ts
@@ -1,4 +1,24 @@
-// survey to edit/show answered
+// domain modelled for FE.
+// On BE domain would look a little bit different, e.g. survey definition (questions) would be separate from
+// answered survey. Here I wanted to simplify things so some layer (domain/loader) would load BE model and
+// would map it to FE model.
+
+// Example of BE model
+
+// SurveyDefinition (here NewSurvey)
+// - id
+// - questionDefinition[] (without answer)
+
+// AnsweredSurvey
+// - id
+// - surveyDefinitionId
+// - answeredQuestion[], where one element:
+// --  questionDefinitionId
+// --  id
+// --  answer / answerId / answerIds
+
+// survey to edit or for showing answered individual survey
+
 export type AnsweredSurveyQuestion =
   | AnsweredSurveySingleAnswerQuestion
   | AnsweredSurveyMultipleAnswerQuestion
