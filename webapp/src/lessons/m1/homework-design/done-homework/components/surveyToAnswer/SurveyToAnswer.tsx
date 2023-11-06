a85efdd556cb380c6a31681a7b06de5a82a60b29
Krzyrok <krzyrok@gmail.com>
2023-11-06 07:27:21 +0100

    add explanation to new survey

--- webapp/src/lessons/m1/homework-design/done-homework/components/surveyToAnswer/SurveyToAnswer.tsx
+++ webapp/src/lessons/m1/homework-design/done-homework/components/surveyToAnswer/SurveyToAnswer.tsx
@@ -30,4 +30,10 @@ const SurveyToAnswerContent: React.FC<SurveyToAnswerContentProps> = (
   </>
 );
 declare const loadNewSurveyToAnswer: (surveyId: string) => NewSurveyDto;
+// I map here to some FormData.
+// For now, I assumed EditDto because I want to add an option to edit answers.
+// If this is not needed, then we can leave NewSurveyDto.
+// Alternatively, we can create SurveyToEditForm but it would be the same as SurveyToEditDto but with optional answers
+// (we can achieve similar thing with union type but it will be a little bit harder of the usage in the code -
+// before `answer` property access, we would need to check type first (if it is Edit or New)).
 declare const mapNewSurveyToFormData: (survey: NewSurveyDto) => SurveyToEditDto;
